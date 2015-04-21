e618898e3e0e861957e7ff3d67c12488a23a3f6e
freepvps <freepvps@gmail.com>
2015-04-21 18:01:40 +0300

    Удалил лишнее

--- testing/compile.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call csc source/checker.cs /out:testing/checker.exe /nologo
-call csc source/tester.cs /out:testing/tester.exe /nologo
-call csc source/invoker.cs /out:testing/invoker.exe /nologo
--- testing/run-tests.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd testing
-call tester %1 ../tests.txt
-pause
