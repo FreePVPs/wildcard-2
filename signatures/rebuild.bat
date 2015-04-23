f40fc71b4e5d084f9903d28d2d73c5eaa8aee74f
freepvps <freepvps@gmail.com>
2015-04-23 22:26:34 +0300

    Изменение пути конечного файла

--- signatures/rebuild.bat
+++ signatures/rebuild.bat
@@ -2,5 +2,5 @@
 cd builder
 call compile.bat
 cd ..
-call "builder\builder.exe" "builder\example.cs" < using-signatures.txt > output.cs
+call "builder\builder.exe" "builder\example.cs" < using-signatures.txt > ..\Hola\signatures\output.cs
 pause
